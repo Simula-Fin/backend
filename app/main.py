2eb321b71b9d79de9d75dd3390e85752dfa5e700
Victor Rayan <victor.rayan378@gmail.com>
2024-04-24 15:12:24 -0300

    feat: add simple auth user with postgrees

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+from app.api.api_router import api_router, auth_router
+from app.core.config import get_settings
+
+app = FastAPI(
+    title="Simula Fin",
+    version="1.0.0",
+    description="https://simula-fin.github.io/DOCS/",
+    openapi_url="/openapi.json",
+    docs_url="/",
+)
+
+app.include_router(auth_router)
+app.include_router(api_router)
+
+# Sets all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        str(origin) for origin in get_settings().security.backend_cors_origins
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Guards against HTTP Host Header attacks
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=get_settings().security.allowed_hosts,
+)
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
